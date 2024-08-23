b854304aa6bc0775c2a677c59617c9312e7c0e42
ArthurVillela <arthur.villela1@gmail.com>
2024-08-23 13:33:41 -0300

    adding sidebar

--- blackscholes.py
+++ blackscholes.py
@@ -45,3 +45,22 @@ def rho(option_type, S, K, r, T, sigma):
 st.set_page_config(layout="wide")
 st.title("Black-Scholes Pricing Model")
 col1, col2 = st.columns(2)
+
+with st.sidebar:
+    st.title("📊 Black-Scholes Model")
+    st.write("`Created by:`")
+    linkedin_url = "https://www.linkedin.com/in/arthur-villela"
+    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Arthur Villela`</a>', unsafe_allow_html=True)
+
+    current_price = st.number_input("Current Asset Price (S)", value=100.0)
+    strike = st.number_input("Strike (K)", value=100.0)
+    time_to_maturity = st.number_input("Years to Maturity (T)", value=1.0)
+    volatility = st.number_input("Volatility (σ)", value=0.2)
+    interest_rate = st.number_input("Risk-Free Interest Rate (r)", value=0.05)
+
+    st.markdown("---")
+    calculate_btn = st.button('Heatmap Parameters')
+    spot_min = st.number_input('Min Spot Price', min_value=0.01, value=current_price*0.8, step=0.01)
+    spot_max = st.number_input('Max Spot Price', min_value=0.01, value=current_price*1.2, step=0.01)
+    vol_min = st.slider('Min Volatility for Heatmap', min_value=0.01, max_value=1.0, value=volatility*0.5, step=0.01)
+    vol_max = st.slider('Max Volatility for Heatmap', min_value=0.01, max_value=1.0, value=volatility*1.5, step=0.01)
