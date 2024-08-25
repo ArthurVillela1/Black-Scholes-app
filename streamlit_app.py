8fea51c6b1a19dff79d84726976b2e85bac8121a
ArthurVillela <arthur.villela1@gmail.com>
2024-08-24 23:13:46 -0300

    new commits

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,24 +4,29 @@ from scipy.stats import norm
 import seaborn as sn
 import matplotlib.pyplot as plt
 
+# d1 calculation
 def d1(S, K, r, T, sigma):
     return (np.log(S/K)+T*(r+np.pow(sigma, 2)/2))/(sigma*np.sqrt(T))
 
+# d2 calculation
 def d2(S, K, r, T, sigma):
     return d1(S, K, r, T, sigma) - sigma*np.sqrt(T)
 
+# Call value calculation through Black-Scholes formula
 def call_value(S, K, r, T, sigma):
     d1_val = d1(S, K, r, T, sigma)
     d2_val = d2(S, K, r, T, sigma)
     BS_call = S*norm.cdf(d1_val)-K*np.exp(-r*T)*norm.cdf(d2_val)
     return BS_call
 
+# Put value calculation through Black-Scholes formula
 def put_value(S, K, r, T, sigma):
     d1_val = d1(S, K, r, T, sigma)
     d2_val = d2(S, K, r, T, sigma)
     BS_put = K*np.exp(-r*T)*norm.cdf(-d2_val)-S*norm.cdf(-d1_val)
     return BS_put
 
+# Greeks calculation
 def delta(option_type, S, K, r, T, sigma):
     if option_type == "call":
         return norm.cdf(d1(S, K, r, T, sigma))
@@ -31,11 +36,13 @@ def delta(option_type, S, K, r, T, sigma):
 def gamma(S, K, r, T, sigma):
     return norm.cdf(d1(S, K, r, T, sigma))/S*sigma*np.sqrt(T)
 
+
 def theta(option_type, S, K, r, T, sigma):
     if option_type == "call":
         return -S*norm.cdf(d1(S, K, r, T, sigma))*sigma/2*np.sqrt(T)-r*K*np.exp(-r*T)*norm.cdf(d2(S, K, r, T, sigma))
     elif option_type == "put":
         return -S*norm.cdf(d1(S, K, r, T, sigma))*sigma/2*np.sqrt(T)+r*K*np.exp(-r*T)*norm.cdf(-d2(S, K, r, T, sigma))
+
 
 def vega(S, K, r, T, sigma):
     return S*np.sqrt(T)*norm.cdf(d1(S, K, r, T, sigma))
@@ -48,13 +55,26 @@ def rho(option_type, S, K, r, T, sigma):
 
 st.set_page_config(layout="wide")
 st.title("Black-Scholes Option Pricing")
+
+expander = st.expander("Learn more")
+expander.write('''
+    The Black & Scholes model is used for pricing options (either puts or calls)
+    through five inputs: underlying asset price (S), option strike price (K), time
+    to maturity (T), standard deviation of the underlying asset (σ) and the risk-free interest rate(r).
+    The formulas for this model are:
+''')
+expander.image("https://media.licdn.com/dms/image/v2/C5612AQFau1WAkESoAQ/article-inline_image-shrink_400_744/article-inline_image-shrink_400_744/0/1589198759071?e=1729123200&v=beta&t=TUE6sKvtGeai4zJfQ_Nx8i79YUn1mU9BoXRPrSCiZUw")
+
 col1, col2 = st.columns(2)
 
+# Getting B&S inputs from the sidebars
 with st.sidebar:
     st.title("📈 Black-Scholes Model")
     st.write("`Created by:`")
     linkedin_url = "https://www.linkedin.com/in/arthur-villela"
     st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Arthur Villela`</a>', unsafe_allow_html=True)
+
+    st.sidebar.write("--------------------------")
 
     s = st.sidebar.number_input("Current Asset Price (S)", value=50.00, step=0.01, min_value=0.0, max_value=9999.00, format="%.2f")
     k = st.sidebar.number_input("Strike (K)", value=70.00, step=0.01, min_value=0.0, max_value=9999.00, format="%.2f")
@@ -62,7 +82,8 @@ with st.sidebar:
     vol = st.sidebar.number_input("Volatility (σ)", value=0.30, step=0.01, min_value=0.0, max_value=9999.00, format="%.2f")
     rf = st.sidebar.number_input("Risk-Free Interest Rate (r)", value=0.15, step=0.01, min_value=0.0, max_value=9999.00, format="%.2f")
 
-def print_value(S, K, r, T, sigma):
+# Printing call and put values
+def fair_values(S, K, r, T, sigma):
     with col1:
         st.header("Call Value")
         st.title(f":blue-background[{round(call_value(S, K, r, T, sigma), 2)}]")
@@ -71,19 +92,19 @@ def print_value(S, K, r, T, sigma):
         st.header("Put Value")
         st.title(f":green-background[{round(put_value(S, K, r, T, sigma), 2)}]")
 
-print_value(s, k, rf, t, vol)
+fair_values(s, k, rf, t, vol)
 
 st.title("Options Heatmap")
-
 col1, col2 = st.columns(2)
 
+# Plotting heatmap
 def heat_map(col, row, title):
-    plt.figure(figsize=(8, 8))  # Increase the size of the heatmap for better visibility
+    plt.figure(figsize=(8, 8))
     if title == "Call":
         sn.heatmap(data=data_call, annot=True, fmt=".2f", cmap="crest", xticklabels=col, yticklabels=row, square=True, cbar_kws={"shrink":0.8})
     else:
         sn.heatmap(data=data_put, annot=True, fmt=".2f", cmap="crest", xticklabels=col, yticklabels=row, square=True, cbar_kws={"shrink":0.8})
-    plt.xlabel("Asset Price")
+    plt.xlabel("Spot Price")
     plt.ylabel("Volatility")
 
     plt.tight_layout(pad=0)
@@ -93,28 +114,26 @@ def heat_map(col, row, title):
 
 st.sidebar.write("--------------------------")
 st.sidebar.subheader("Heatmap Parameters")
-min_vol = st.sidebar.slider("Min volatility", 0.01, 1.00, vol)
+
+# Getting the heatmap parameters from the sidebar
+min_vol = st.sidebar.slider("Min Volatility", 0.01, 1.00, vol)
 max_vol = st.sidebar.slider("Max Volatility", 0.01, 1.00, vol*2)
-min_price = round(st.sidebar.number_input("Min Price", value=round(s*0.8, 2), step=0.01, min_value=0.0, max_value=9999.00, format="%.2f"), 2)
-max_price = round(st.sidebar.number_input("Max Price", value=round(s*1.2, 2), step=0.01, min_value=0.0, max_value=9999.00, format="%.2f"), 2)
+min_price = round(st.sidebar.number_input("Min Price", value=round(s*0.5, 2), step=0.01, min_value=0.0, max_value=9999.00, format="%.2f"), 2)
+max_price = round(st.sidebar.number_input("Max Price", value=round(s*1.5, 2), step=0.01, min_value=0.0, max_value=9999.00, format="%.2f"), 2)
 
-# Fixing the number of rows and columns for consistent heatmap dimensions
-fixed_rows = 10
-fixed_columns = 10
+# Creating evenly spaced numbers between min_vol and max_vol
+rows = np.linspace(min_vol, max_vol, 10)
+columns = np.linspace(min_price, max_price, 10)
 
-# Creating the values to multiply for the heatmap
-rows = np.linspace(min_vol, max_vol, fixed_rows)
-columns = np.linspace(min_price, max_price, fixed_columns)
-
-# Round the axis labels to two decimal places for better readability
+# Rounding numbers for better layout
 rows_print = [round(x, 2) for x in rows]
 columns_print = [round(x, 2) for x in columns]
 
-# Creating the 2D matrices for the heat maps
+# Creating matrices for the heat maps
 data_call = [[call_value(S, k, rf, t, sigma) for S in columns] for sigma in rows]
 data_put = [[put_value(S, k, rf, t, sigma) for S in columns] for sigma in rows]
 
-# Outputting the heatmaps to the screen
+# Heatmap output
 with col1:
     st.header("Call")
     heat_map(columns_print, rows_print, "Call")
@@ -127,6 +146,7 @@ st.title("Greeks")
 
 col1, col2 = st.columns(2)
 
+# Plotting greeks
 with col1:
     st.header("Call")  
     st.subheader(f"**Delta (∆):** :blue-background[{round(delta('call',s, k, rf, t, vol), 3)}]")
